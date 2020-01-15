4c0d4d28d89986dcc18318b6ad33a72778edc90d
Craig Clayton <mancityvsliverpoolfc@outlook.com>
2020-01-15 19:37:58 +0000

    Changed links to green, index.js

--- js/index.js
+++ js/index.js
@@ -50,4 +50,8 @@ navItems[1].textContent = siteContent['nav']['nav-item-2'];
 navItems[2].textContent = siteContent['nav']['nav-item-3'];
 navItems[3].textContent = siteContent['nav']['nav-item-4'];
 navItems[4].textContent = siteContent['nav']['nav-item-5'];
-navItems[6].textContent = siteContent['nav']['nav-item-6'];
+navItems[5].textContent = siteContent['nav']['nav-item-6'];
+
+for (i = 0; i < navItems.length; i++) {
+  navItems[i].style.color = "green";
+}
